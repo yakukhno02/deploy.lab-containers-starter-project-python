7905a5563c0a75fafd09bf01fd65b576e7f548e3
ykmshhh <yakukhno.maria@lll.kpi.ua>
2026-05-03 17:16:12 +0300

    add numpy and switch base image to python:3.13-alpine

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,15 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/matrix")
+def matrix():
+    matrix_a = np.random.randint(0, 10, (10, 10))
+    matrix_b = np.random.randint(0, 10, (10, 10))
+    product = np.dot(matrix_a, matrix_b)
+
+    return {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist(),
+    }
